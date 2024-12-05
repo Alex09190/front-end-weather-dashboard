912be3bef8a62c3b2a0b5585f88b504f55b1a79c
SacredNoob <tuk12963@temple.edu>
2024-12-04 21:30:00 -0500

    finished search component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Search from "./Search/Search";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Search />}
+          ></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/Search/Search.jsx
+++ src/Search/Search.jsx
@@ -2,34 +2,99 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Search = () => {
-  const apiEndpoint = process.env.REACT_API_URL;
+  const apiEndpoint = process.env.REACT_APP_API_URL_LOCATION;
 
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("NYC");
+  const [cities, setCities] = useState(null);
   const [location, setLocation] = useState(null);
-  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleInput = (event) => {
+    if (event) {
+      event.preventDefault(); //disable submiting form on pressing enter
+    }
     setSearchQuery(event.target.value);
   };
 
-  const fetchLocation = (event) => {
+  const fetchCities = (event) => {
     if (event) {
-      event.preventDefault(); //disable button functionality
+      event.preventDefault(); //not needed, api call on happens on handleInput
     }
+    console.log(searchQuery);
     fetch(`${apiEndpoint}?q=${searchQuery}`) //fetch article data via proxy server endpoint with searchable filters
       .then((response) => {
         if (!response.ok) throw new Error(`Error fetching data`);
         return response.json();
       })
       .then((data) => {
-        setArticles(data.response.docs);
+        setCities(data);
       })
       .catch((error) => {
-        console.error(error.message);
+        setError(error.message);
       });
   };
 
-  return <div></div>;
+  const selectCity = (event) => {
+    //puts specific city information from clicked link into session storage
+    const city = event.target;
+
+    cities.map((cities) => {
+      //check for selected article from searched ones, and store into storage
+      const cityCheck = cities.state
+        ? `${cities.name}, ${cities.state}, ${cities.country}`
+        : `${cities.name}, ${cities.country}`;
+      if (cityCheck === city.innerText) {
+        setLocation({
+          name: cities.name,
+          country: cities.country,
+          state: cities.state,
+          lat: cities.lat,
+          lon: cities.lon,
+        });
+
+        sessionStorage.setItem("location", JSON.stringify(location));
+      }
+    });
+  };
+
+  //initialize and api call when search is changed
+  useEffect(() => {
+    fetchCities();
+  }, [searchQuery]);
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <p>Search for a city and then click a location</p>
+      <form onSubmit={fetchCities}>
+        <input
+          type="text"
+          name="searchQuery"
+          id="searchQuery"
+          onChange={handleInput}
+        />
+      </form>
+
+      {cities && (
+        <div>
+          {cities.map((cities, index) => (
+            //print city names, states, and countries - some cities aren't in a state => ternary conditional
+            <div
+              key={index}
+              onClick={selectCity}
+            >
+              <h2>
+                {cities.name}, {cities.state ? `${cities.state}, ` : ""}
+                {cities.country}
+              </h2>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
